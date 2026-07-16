51c78217bcbf6ae741385d43eb0721af55162342
Developer <developer@wacrm.com>
2026-07-16 15:10:44 +0530

    Fix test2.ts typescript error

--- test2.ts
+++ test2.ts
@@ -2,7 +2,7 @@ import { getFlowTemplate } from './src/lib/flows/templates';
 import { deriveCanvasEdges } from './src/lib/flows/edges';
 
 const template = getFlowTemplate('faq_bot');
-const builderNodes = template?.nodes.map(n => ({ ...n, position_x: 0, position_y: 0 })) ?? [];
+const builderNodes = (template?.nodes.map(n => ({ ...n, position_x: 0, position_y: 0 })) ?? []) as any;
 const edges = deriveCanvasEdges(builderNodes);
 
 console.log('edges:', edges);
